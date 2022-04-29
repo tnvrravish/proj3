cd0ec5cb04fa987574a97856f242f6258f742b58
tnvrravish <vt7@njit.edu>
2022-04-29 18:42:42 -0400

    Added Genre and year

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -12,12 +12,16 @@ class Song(db.Model,SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(300), nullable=True, unique=False)
     artist = db.Column(db.String(300), nullable=True, unique=False)
+    genre = db.Column(db.String(300), nullable=True, unique=False)
+    year = db.Column(db.Integer, nullable=True, unique=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     user = relationship("User", back_populates="songs", uselist=False)
 
-    def __init__(self, title, artist):
+    def __init__(self, title, artist, genre, year):
         self.title = title
         self.artist = artist
+        self.genre = genre
+        self.year = year
 
 class Location(db.Model, SerializerMixin):
     __tablename__ = 'locations'
--- tests/user_test.py
+++ tests/user_test.py
@@ -24,7 +24,7 @@ def test_adding_user(application):
         #asserting that the user retrieved is correct
         assert user.email == 'keith@webizly.com'
         #this is how you get a related record ready for insert
-        user.songs= [Song("test","smap"),Song("test2","te")]
+        user.songs= [Song("test","smap","rap",2012),Song("test2","te","pop",2011)]
         #commit is what saves the songs
         db.session.commit()
         assert db.session.query(Song).count() == 2
