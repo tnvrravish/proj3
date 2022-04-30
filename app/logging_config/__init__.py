2a58700fc9375ea936c89ff3591554913bb33848
tnvrravish <vt7@njit.edu>
2022-04-30 12:46:04 -0400

    request and response log

--- app/logging_config/__init__.py
+++ app/logging_config/__init__.py
@@ -7,7 +7,6 @@ from flask import request, current_app
 
 # from app.logging_config.log_formatters import RequestFormatter
 from app import config
-
 log_con = flask.Blueprint('log_con', __name__)
 
 
@@ -54,11 +53,6 @@ LOGGING_CONFIG = {
         'req': {
             'format': '%(asctime)s : %(message)s'
         },
-        'RequestFormatter': {
-            '()': 'app.logging_config.log_formatters.RequestFormatter',
-            'format': '[%(asctime)s] [%(process)d] %(remote_addr)s requested %(url)s'
-                      '%(levelname)s in %(module)s: %(message)s'
-        },
 
     },
     'handlers': {
@@ -77,7 +71,7 @@ LOGGING_CONFIG = {
         },
         'file.handler.myapp': {
             'class': 'logging.handlers.RotatingFileHandler',
-            'formatter': 'RequestFormatter',
+            'formatter': 'req',
             'filename': os.path.join(config.Config.LOG_DIR, 'myapp.log'),
             'maxBytes': 10000000,
             'backupCount': 5,
