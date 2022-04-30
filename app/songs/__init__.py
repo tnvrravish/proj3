93667c37c63530886b5d753504c12ccdeff4a7cf
tnvrravish <vt7@njit.edu>
2022-04-30 11:57:08 -0400

    request and response log

--- app/songs/__init__.py
+++ app/songs/__init__.py
@@ -31,7 +31,7 @@ def songs_browse(page):
 def songs_upload():
     form = csv_upload()
     if form.validate_on_submit():
-        log = logging.getLogger("csvlog")
+        log = logging.getLogger("mycsv")
 
         filename = secure_filename(form.file.data.filename)
         filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
