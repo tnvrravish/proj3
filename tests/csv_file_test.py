f03f0c611af352405109384eb42a58ff924816eb
tnvrravish <vt7@njit.edu>
2022-04-30 13:44:44 -0400

    request and response log

--- tests/csv_file_test.py
+++ tests/csv_file_test.py
@@ -14,4 +14,4 @@ def test_csv_upload(client, add_user, application):
             csv_file = csv.DictReader(file)
             for row in csv_file:
                 testvar = row
-            assert testvar == {'Artist': 'xyz', 'Genre': 'pop', 'Name': 'abc', 'Year': '2022'}
+            assert testvar == {'Artist': 'Vishnu', 'Genre': 'jazz', 'Name': 'Speachless', 'Year': '2012'}
