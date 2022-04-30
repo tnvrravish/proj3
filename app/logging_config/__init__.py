b4200ba0c8aafdb7a6e0528c94afa148544b5d15
tnvrravish <vt7@njit.edu>
2022-04-30 03:50:50 -0400

    request and response log

--- app/logging_config/__init__.py
+++ app/logging_config/__init__.py
@@ -5,7 +5,7 @@ from logging.config import dictConfig
 import flask
 from flask import request, current_app
 
-from app.logging_config.log_formatters import RequestFormatter
+# from app.logging_config.log_formatters import RequestFormatter
 from app import config
 
 log_con = flask.Blueprint('log_con', __name__)
